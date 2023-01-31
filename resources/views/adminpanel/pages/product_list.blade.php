2f57f6595e0fa042f43ea4b5d298161dc18562b7
Maria <mianhamza7262@gmail.com>
2023-02-01 00:18:23 +0500

    fsd

--- resources/views/adminpanel/pages/product_list.blade.php
+++ resources/views/adminpanel/pages/product_list.blade.php
@@ -87,7 +87,7 @@ der-bottom white-bg page-heading">
                                     @foreach ($products as $product)
                                         <tr class="gradeX" id="row-{{ $product->id }}"
                                             @if ($product->available_qty < 5)
-                                            style="background-color: rgb(206, 176, 171)"
+                                            style="background-color: rgb(244, 215, 210)"
                                             @endif
                                             >
                                             <td>{{ $counter }}</td>
